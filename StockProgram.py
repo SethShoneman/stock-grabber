6b46a91ea7021331b3e286cb55f150321d4a789b
Seth Shoneman <60634819+SethShoneman@users.noreply.github.com>
2021-04-20 11:04:38 -0400

    Changed output format and solved stock discrepancy
    For certain symbols, the output would be inconsistent and thus the CSV file would not be rectangularly displayable. Will now populate empty and nonexistent cells with "N/A", and will add extra column to account for the "assetSubType" value that some stocks have.

--- StockProgram.py
+++ StockProgram.py
@@ -2,7 +2,6 @@ import requests
 import json
 from datetime import datetime
 
-# grab information on given stocks, and create a wanted number of files containing that information
 def stockFunc(num,stocks):
     for x in range(int(num)):
         #gets data from API
@@ -18,8 +17,29 @@ def stockFunc(num,stocks):
         for stock in data:
             stockList.append(stock)
 
-        #gets all stock values contained together, separated by stock, into a list
         values = []
+
+        # check for if an offset exists
+        offsetError = False
+        for symbol in stockList:
+            elementNum = 1
+            for val in data[symbol].keys():
+                if(elementNum == 4 and val == "symbol"):
+                    offsetError = True
+                elementNum += 1
+
+        # initially populates value two dimensional array with titles for easier csv reading
+        stockTitles = []
+        elementNum = 1
+        for val in data[stockList[0]].keys():
+            if(offsetError and elementNum == 4 and val == "symbol"):
+                stockTitles.append("assetSubType")
+            stockTitles.append(val)
+            elementNum += 1
+        values.append(stockTitles)
+
+
+        # gets all stock values contained together, separated by stock, into a list
         for symbol in stockList:
             stockValues = []
             for val in data[symbol].values():
@@ -27,14 +47,27 @@ def stockFunc(num,stocks):
             values.append(stockValues)
 
         #changes name of file
-        #this is where I placed the files, the path would be changed to where output files are wanted         
         name = 'C:\\Users\\sidda\\Documents\\ameritrade\\outputFiles'+"\\output"+str(x+1)+".csv"
         print(name)
 
-        #writes final values to a csv file
+        #writes final values to a csv file, initially creating a title row for easier use
         with open(name, 'w') as f:
-            for stock in values:
-                for val in stock:
-                    f.write("\""+str(val)+"\",")
+            for title in values[0]:
+                f.write("\"" + str(title) + "\",")
+            f.write("\"dateAndTime\"")
+            f.write("\n")
+            for row in values[1:]:
+                elementNum2 = 1
+                for val in row:
+                    # N/A if doesnt have assetSubtype to correct for offset
+                    if(val == "" or val == " "):
+                        f.write("\"N/A\",")
+                    elif(offsetError == True and len(row) == 48 and elementNum2 == 4):
+                        f.write("\"N/A\",")
+                        f.write("\"" + str(val) + "\",")
+                    # N/A if there is not an available value
+                    else:
+                        f.write("\""+str(val)+"\",")
+                    elementNum2 += 1
                 f.write("\""+str(datetime.now())+"\"")
                 f.write("\n")
